47ae6a4d25f5d0fc576f3156b7184f61270745a9
Karthi-KeyanG <karthi47205@gmail.com>
2026-06-17 16:01:13 +0000

    Initial check

---
+++ menu.c
@@ -0,0 +1,24 @@
+// menu card for the Hotel
+#include<stdio.h>
+int main()
+    {
+         char op;
+
+         printf("--------Welcome to the Hotel--------\n\n");
+         printf("Select what you want->\n");
+         printf("1. starter\n2. Main course\n3. Dessert\n4. Exit\n");
+
+         scanf("%c",&op);
+
+         switch(op)
+         {
+            case '1':
+            printf("You have choosed starter\n");
+            printf("1. Chicken 65\n2. Dragon chicken\n3. Paneer pakoda");
+            break;
+
+            case '2':
+            printf("1. \n2. \n 3. \n");
+            
+         }
+    }
